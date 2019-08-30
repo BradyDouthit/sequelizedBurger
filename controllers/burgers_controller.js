79a308b901104219188662442bacfbc039c7cf76
Brady Douthit <brady.douthit99@gmail.com>
2019-08-30 16:13:09 -0400

    The first burger now shows on screen

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,24 +4,41 @@ let db = require("../models")
 module.exports = function (app) {
   //handlebars routes
   app.get('/', function (req, res) {
-    res.render("index")
+    db.burger.create({
+      burger_name: "classic cheeseburger"
+    });
+    db.burger.create({
+      burger_name: "Bacon cheeseburger"
+    })
+    db.burger.findAll({})
+      .then(function (data) {
+
+        for (var i = 0; i < data.length; i++) {
+          var burgerName = data[i].burger_name
+          console.log(burgerName)
+        };
+
+        res.render("index", {
+          burgers: burgerName
+        });
+      })
   });
 
 
 
   //api routes
-  app.get('/api/burgers', function(req, res) {
+  app.get('/api/burgers', function (req, res) {
     db.burger.findAll({})
-    .then(function(dbBurger) {
-      console.log(dbBurger)
-      res.json(dbBurger)
-    });
+      .then(function (dbBurger) {
+        //console.log(dbBurger)
+        res.json(dbBurger)
+      });
   });
 
-  app.post('/api/burgers', function(req, res) {
-    db.burger.create(req.body).then(function(dbBurger) {
-      console.log(req.body)
-      console.log(req.params)
+  app.post('/api/burgers', function (req, res) {
+    db.burger.create(req.body).then(function (dbBurger) {
+      // console.log(req.body)
+      // console.log(req.params)
       res.json(dbBurger)
     });
   });
--- public/assets/burger.js
+++ public/assets/burger.js
@@ -2,10 +2,14 @@ $('.submit-button').on('click', function(event) {
     event.preventDefault();
     let burgerName = {"burger_name": $(".burger-name").val()};
     
-    console.log(`Burger name: ${burgerName.burger_name}`);
+    //console.log(`Burger name: ${burgerName.burger_name}`);
 
     //posts the burger name
-    $.post('/api/burgers', burgerName, function(){
+    $.post('/api/burgers', burgerName, function(data){
+        console.log(data)
         $('.burger-name').val('')
+    }).then(function(data) {
+        location.reload();
+        console.log(data)
     })
 })
