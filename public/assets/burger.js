987720072a4ee0d135a5e4add9dac06d89bdfb0b
Brady Douthit <brady.douthit99@gmail.com>
2019-09-02 20:02:24 -0400

    tweaked postBurger so it now reloads the page

--- public/assets/burger.js
+++ public/assets/burger.js
@@ -1,13 +1,9 @@
-//$(document).ready(function () {
-
 var burgers = [];
 
 
 
 $('.submit-button').on('click', function (event) {
-    event.preventDefault();
     postBurger();
-    getBurgers();
 });
 
 
@@ -40,22 +36,18 @@ function getBurgers() {
 };
 
 
-//posts a burger when the user enters a name and clicks submit
+//posts a burger, and afterwards gets all burgers and reloads the page
 function postBurger() {
     let burgerValues = {
         'burger_name': $('.burger-name').val(),
         'devoured': false
     };
 
-    //console.log(`Burger name: ${burgerName.burger_name}`);
-
     //posts the burger name
     $.post('/api/burgers', burgerValues, function () {
         $('.burger-name').val('')
-    }).then(function () {
+    }).then(function() {
         getBurgers();
-    });
+        location.reload();
+    })
 };
-
-
-//});
